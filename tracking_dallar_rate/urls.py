ed565d4f6721ede4bf0985f3f5d3f14b83bd7db5
Konstantin <goduskospan@gmail.com>
2024-11-07 09:29:27 +0500

    Add: get_current_usd endpoint

--- tracking_dallar_rate/urls.py
+++ tracking_dallar_rate/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('get-current-usd/', views.get_current_usd),
 ]
