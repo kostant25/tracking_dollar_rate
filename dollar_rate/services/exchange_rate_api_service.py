2de3f4ba69b1a02dd42b27894c7baabf10841f98
Konstantin <goduskospan@gmail.com>
2024-11-07 12:59:40 +0500

    Update: add create and get 11 rate

--- dollar_rate/services/exchange_rate_api_service.py
+++ dollar_rate/services/exchange_rate_api_service.py
@@ -1,12 +1,23 @@
 import requests as api_req
-from datetime import datetime
 
+from dollar_rate.services.service import add_new_rate, get_last_count_rates
 from tracking_dallar_rate import settings
 
 
-def get_current_usd_service() -> dict[str, str]:
-    data = {
-        'rate': api_req.get(settings.EXCHANGE_RATE_API_LINK).json()['conversion_rates']['RUB'],
-        'time': datetime.now().strftime('%m/%d/%Y %H:%M:%S'),
-    }
+def get_current_usd_service() -> list[dict[str, str]]:
+    """Получение нового курса и 10 последних"""
+    # add_new_rate(api_req.get(settings.EXCHANGE_RATE_API_LINK).json()['conversion_rates']['RUB'])
+
+    add_new_rate(98.2)
+
+    rates = get_last_count_rates(11)
+
+    data = []
+
+    for rate in rates:
+        data.append({
+            "rate": rate.usd_to_rub_rate,
+            "date": rate.date_time_request
+        })
+
     return data
