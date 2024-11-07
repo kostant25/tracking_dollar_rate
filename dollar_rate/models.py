274b43f5c37559188f49423292196fdf19ce5aca
Konstantin <goduskospan@gmail.com>
2024-11-07 12:58:23 +0500

    Update: DollarRate model: auto_now_add

--- dollar_rate/models.py
+++ dollar_rate/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class DollarRateHistory(models.Model):
     usd_to_rub_rate = models.FloatField()
-    date_time_request = models.DateTimeField()
+    date_time_request = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.date_time_request)
