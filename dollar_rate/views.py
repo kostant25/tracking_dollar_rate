3aeebd30607316c8e6a203d99d35beab775d33af
Konstantin <goduskospan@gmail.com>
2024-11-07 12:12:58 +0500

    Refactor: views in dollar_rate.views

--- dollar_rate/views.py
+++ dollar_rate/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+
+from .services.exchange_rate_api_service import get_current_usd_service
+
+
+def get_current_usd(req) -> JsonResponse:
+
+    data = get_current_usd_service()
+
+    return JsonResponse({"status": "OK", "data": [data]})
