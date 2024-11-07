227ee6f93049c0c6aacae478d717bc7f16a4a9c7
Konstantin <goduskospan@gmail.com>
2024-11-07 12:59:56 +0500

    Update: add doc string

--- dollar_rate/views.py
+++ dollar_rate/views.py
@@ -6,7 +6,7 @@ from .services.exchange_rate_api_service import get_current_usd_service
 
 
 def get_current_usd(req) -> JsonResponse:
-
+    """Получение текущего курса и последних 10 запросов"""
     data = get_current_usd_service()
 
-    return JsonResponse({"status": "OK", "data": [data]})
+    return JsonResponse({"status": "OK", "data": data})
